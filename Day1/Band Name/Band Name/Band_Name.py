cf7713b60c52627862383a1a51ee38197c20bf2d
Negm-elgaan <ohossam962@gmail.com>
2025-01-02 13:11:04 +0200

    First of first day

---
+++ Day1/Band Name/Band Name/Band_Name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+City = input("What is the name of the city you grew in?\n")
+Pet = input("What's the name of your pet?\n")
+print("Your Band name could be" , City , Pet)
